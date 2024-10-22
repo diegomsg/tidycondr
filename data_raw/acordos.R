27167e295edb8c70869129f74448531cb8c9c2fe
Diego Guimarães <diegomsg@gmail.com>
2024-10-22 00:36:43 -0300

    anonymised data for examples

---
+++ data_raw/acordos.R
@@ -0,0 +1,24 @@
+library(tidyxl)
+library(unpivotr)
+
+# raw xlsx from restricted xlsx
+acordos_raw <- xlsx_cells("ex/acordos.xlsx")
+
+# header rows to skip
+skip_rows <- c(
+  2:5, 7:10, 26:44, 46:49, 62:74, 76:79, 83:92, 94:97, 114:124, 126:129,
+  134:140)
+
+# anonymise
+acordos_anonym <- anonymise(acordos_raw, skip_rows)
+acordos_anonym <- rectify(acordos_anonym)
+acordos_anonym <- acordos_anonym[-1]
+
+# write
+writexl::write_xlsx(
+  acordos_anonym,
+  path = "data_raw/acordos.xlsx",
+  col_names = FALSE,
+  format_headers = FALSE)
+
+usethis::use_data(acordos_anonym, overwrite = TRUE)
---
+++ data_raw/pcontas.R
@@ -0,0 +1,24 @@
+library(tidyxl)
+library(unpivotr)
+
+# raw xlsx from restricted xlsx
+pcontas_raw <- xlsx_cells("ex/pcontas.xlsx")
+
+# header rows to skip
+skip_rows <- c(
+  2:4, 12:14, 16, 89, 91, 229, 231, 233, 235:237, 252:254, 264:266, 389,
+  391:400, 402:403, 405:406, 408:410)
+
+# anonymise
+pcontas_anonym <- anonymise(pcontas_raw, skip_rows)
+pcontas_anonym <- rectify(pcontas_anonym)
+pcontas_anonym <- pcontas_anonym[-1]
+
+# write
+writexl::write_xlsx(
+  pcontas_anonym,
+  path = "data_raw/pcontas.xlsx",
+  col_names = FALSE,
+  format_headers = FALSE)
+
+usethis::use_data(pcontas_anonym, overwrite = TRUE)
