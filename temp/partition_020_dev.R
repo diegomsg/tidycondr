cf3864a7111f62d3deaa1c8e0ebd2dae8e529673
diegomsg <diegomsg@gmail.com>
2024-11-30 11:57:23 -0300

    dev partition_020()

--- temp/partition_020_dev.R
+++ temp/partition_020_dev.R
@@ -4,6 +4,8 @@ pcontas_part <- pcontas_part[grepl("020", pcontas_part$code),]
 
 tbl <- pcontas_part[1,4][[1]][[1]]
 
+rec_tbl <- rectify(tbl)
+
 # main corners rows
 summary_rows <- get_partition_corners_rows(
   tbl, filled_cols = 1:2, empty_cols = 3:6)
@@ -32,17 +34,45 @@ summ_tbl <- tbl |>
 
 # details rows
 # get receitass e despesas block
-head_values <- c("Receitas", "Competência", NA, "Valor")
-head_cols <- length(head_values)
+
+## head row
+head_values <- list(
+  c("Receitas", "Competência", NA, "Valor"),
+  c("Despesas",	"Liquidação", "Documento",	"Forma de Pgto.",	NA,	"Valor"))
+
+head_cols <- sapply(head_values, length) |>
+  max()
 names(head_values) <- names(rec_tbl)[1:head_cols + 1]
+
+# aux funs
+
+vec_to_tibl <- function(vec, names_from_tbl) {
+  vec_names <- names(names_from_tbl)[1:length(vec) + 1]
+  names(vec) <- vec_names
+  t(vec) |>
+    as_tibble()
+}
+
+head_rows <- function(head_values, tbl_to_extract_rows) {
+  head_values |>
+    lapply(vec_to_tibl, tbl_to_extract_rows) |>
+    bind_rows() |>
+    inner_join(tbl_to_extract_rows) |>
+    pull(last_col())
+}
+
+head_values |>
+  head_rows(rec_tbl)
+
+lapply(head_values, t)
 
 head_tbl <- t(head_values) |>
   as_tibble()
 
 #from here
+inner_join(head_tbl,
+  rec_tbl) |>
 
-inner_join(
-  rec_tbl)
 
 tbl[tbl$character == "Despesas",] |>
   rectify()
