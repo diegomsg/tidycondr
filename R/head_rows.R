f680642ccf005840bb5cdc59a9086fa1752672df
Diego Guimarães <diegomsg@gmail.com>
2024-12-01 20:40:50 -0300

    partition_020 dev

---
+++ R/head_rows.R
@@ -0,0 +1,46 @@
+#' Retrive Column Names from Tibble
+#'
+#' @param vec Character vector sequence of cell values in row
+#' @param names_from_tbl Tibble to retrive column names from
+#'
+#' @return Tibble with col names and values in vector.
+#'
+vec_to_tibl <- function(vec, names_from_tbl) {
+  vec_names <- names(names_from_tbl)[1:length(vec) + 1]
+  names(vec) <- vec_names
+  t(vec) |>
+    as_tibble()
+}
+
+#' Rows Indices for Sequence Cell Values
+#'
+#' @param head_values Vector or list of vectors
+#' @param tbl_to_extract_rows Tibble to retrive column names from
+#'
+#' @return Rows indices numeric vector
+#'
+head_rows_base <- function(head_values, tbl_to_extract_rows) {
+  head_values |>
+    lapply(vec_to_tibl, tbl_to_extract_rows) |>
+    bind_rows() |>
+    inner_join(tbl_to_extract_rows) |>
+    pull(last_col())
+}
+
+#' Rows Indices for Sequence Cell Values
+#'
+#' @param head_values Vector or list of vectors
+#' @param tbl_to_extract_rows Tibble to retrive column names from
+#' @param verbose Verbose for `dplyr::inner_join()` messages
+#'
+#' @return Rows indices numeric vector
+#' @export
+#'
+head_rows <- function(head_values, tbl_to_extract_rows, verbose = TRUE) {
+  if(verbose) {
+    head_rows_base(head_values, tbl_to_extract_rows)
+  } else {
+    suppressMessages(
+      head_rows_base(head_values, tbl_to_extract_rows))
+  }
+}
--- temp/partition_020_dev.R
+++ temp/partition_020_dev.R
@@ -1,4 +1,4 @@
-pcontas <- read_contas("data_raw/pcontas.xlsx")
+pcontas <- read_contas("ex/pcontas.xlsx")
 pcontas_part <- partition_contas(pcontas)
 pcontas_part <- pcontas_part[grepl("020", pcontas_part$code),]
 
@@ -40,59 +40,61 @@ head_values <- list(
   c("Receitas", "Competência", NA, "Valor"),
   c("Despesas",	"Liquidação", "Documento",	"Forma de Pgto.",	NA,	"Valor"))
 
-head_cols <- sapply(head_values, length) |>
-  max()
-names(head_values) <- names(rec_tbl)[1:head_cols + 1]
-
-# aux funs
-
-vec_to_tibl <- function(vec, names_from_tbl) {
-  vec_names <- names(names_from_tbl)[1:length(vec) + 1]
-  names(vec) <- vec_names
-  t(vec) |>
-    as_tibble()
-}
-
-head_rows <- function(head_values, tbl_to_extract_rows) {
-  head_values |>
-    lapply(vec_to_tibl, tbl_to_extract_rows) |>
-    bind_rows() |>
-    inner_join(tbl_to_extract_rows) |>
-    pull(last_col())
-}
-
-head_values |>
-  head_rows(rec_tbl)
-
-lapply(head_values, t)
-
-head_tbl <- t(head_values) |>
-  as_tibble()
-
-#from here
-inner_join(head_tbl,
-  rec_tbl) |>
-
-
-tbl[tbl$character == "Despesas",] |>
-  rectify()
-
-get_partition_corners_rows(
-  tbl, filled_cols = c(1:2, 4), empty_cols = 3)
-
-tbl |>
-  rectify() |>
-  View()
+head_rows <- head_rows(head_values, rec_tbl, verbose = FALSE)
 
 # partition data
-corners_main <- tbl |>
+partition_cells <- tbl[tbl$row %in% head_rows & tbl$col == 1L,]
+
+nested_info <- partition(tbl, partition_cells) |>
+  select(info = character, cells)
+
+first_lvl_group_pat <- " \\([0-9]{1,3},?[0-9]*\\%\\)$"
+first_lvl_group_rows <-
+  tbl[tbl$col == 1
+      & grepl(first_lvl_head_pat, tbl$character),]
+
+second_lvl_group_rows_id <- get_partition_corners_rows(tbl, 1, 2:6)
+second_lvl_group_rows <-
+  tbl[tbl$col == 1
+      & tbl$row %in% second_lvl_group_rows_id
+      & !(tbl$row %in% first_lvl_group_rows$row),]
+
+rec_resum <- nested_info$cells[1][[1]] |>
+  behead("up", "head") |>
+  tidyr::replace_na(list(head = "prop_subgrupo")) |>
+  partition(first_lvl_group_rows) |>
+  select(grupo = character, cells) |>
+  tidyr::unnest(cells) |>
+  filter(!(row %in% first_lvl_group_rows$row)) |>
+  partition(second_lvl_group_rows) |>
+  select(subgrupo = character, cells) |>
+  tidyr::unnest(cells) |>
   filter(
-    row %in% summary_rows,
-    col == 1L)
-
-partition(tbl, corners = corners_main, align = "top_left") |>
-  View()
-
+    !(row %in% first_lvl_group_rows$row)
+    & !(row %in% second_lvl_group_rows$row)) |>
+  mutate(
+    value = coalesce(
+      character,
+      as.character(numeric))) |>
+  select(-c(col:character)) |>
+  pivot_wider(
+    names_from = head,
+    values_from = value) |>
+  select(-row) |>
+  janitor::clean_names() |>
+  tidyr::separate_wider_regex(
+    grupo,
+    c(
+      grupo = ".*",
+      part_grupo = first_head_pat)) |>
+  mutate(
+    part_grupo = parse_number(
+      part_grupo,
+      locale = locale(
+        decimal_mark = ",",
+        grouping_mark = ".")),
+    grupo_sum_row = receitas == paste0("Total de ", grupo),
+    subgrupo_sum_row = receitas == paste0("Total de ", subgrupo))
 
 # return
 tibble(
