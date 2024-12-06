b23f7cb0adcf9b2d69e7127715e237b824e56dd2
Diego Guimarães <diegomsg@gmail.com>
2024-12-05 22:51:05 -0300

    new function `assert_tidyxl()` and doc

--- R/check_tidyxl.R
+++ R/check_tidyxl.R
@@ -12,11 +12,31 @@
 #'
 #' acordos <- read_contas("data_raw/acordos.xlsx")
 #' check_tidyxl(acordos)
-
+#'
 check_tidyxl <- function(tbl) {
   needed_cols <- c(
     "row", "col", "is_blank", "content", "data_type", "error", "logical",
     "numeric", "date", "character")
 
   all(needed_cols %in% names(tbl))
+}
+
+#' Assert if  it is a tidyxl tibble
+#'
+#' If it`s not a simple [tidyxl::xlsx_cells()] with selected cols,
+#'   stop execution.
+#'
+#' @param tbl tibble
+#'
+#' @return stop or not
+#'
+#' @examples
+#' pcontas <- read_contas("data_raw/pcontas.xlsx")
+#' assert_tidyxl(pcontas)
+#'
+#' acordos <- read_contas("data_raw/acordos.xlsx")
+#' assert_tidyxl(acordos)
+#'
+assert_tidyxl <- function(tbl) {
+  stopifnot("Not a tidyxl tibble." = check_tidyxl(tbl))
 }
