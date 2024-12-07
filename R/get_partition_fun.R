e2a5be99b62a40aea51a21cb9db68b074c1976a2
diegomsg <diegomsg@gmail.com>
2024-12-07 12:40:11 -0300

    fix to enable 020_receitas || 020_despesass call

--- R/get_partition_fun.R
+++ R/get_partition_fun.R
@@ -12,7 +12,12 @@
 #' partition_function("016B")(list(parts[1, 4][[1]][[1]]))
 #'
 get_partition_fun <- function(str) {
-  fun_code <- strtrim(str, 3)
+  fun_code <- if (
+    c("Receitas$", "Despesas$") |>
+      sapply(grepl, str, TRUE, USE.NAMES = FALSE) |>
+      any()
+    ) {str}
+  else {strtrim(str, 3)}
   fun_name <- paste0("partition_", fun_code)
   function(x) {
     do.call(fun_name, x)
