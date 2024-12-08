6c940380e579fffb18202efafa0c771eac5fd00d
Diego Guimarães <diegomsg@gmail.com>
2024-12-08 18:03:30 -0300

    finish partition_020 dev

--- temp/partition_020_dev.R
+++ temp/partition_020_dev.R
@@ -1,30 +1,5 @@
-pcontas <- read_contas("data_raw/pcontas.xlsx")
+pcontas <- read_contas("ex/pcontas.xlsx")
 pcontas_part <- partition_contas(pcontas)
 pcontas_part <- pcontas_part[grepl("020", pcontas_part$code),]
 
-tbl <- pcontas_part[1,4][[1]][[1]]
-
-partition_020_summary(tbl)
-partition_020_groups(tbl) |>
-  filter(info == "Receitas") |>
-  mutate(code = glue::glue("020_{info}")) |>
-  call_partition_funs(
-    .cells_col = data)
-
-# error in get_partition_corners_rows, to much cols
-
-# return
-resumo <- tibble(
-  "chapter" = c("Resumo movimentação"),
-  "data" = list(
-    partition_020_summary(tbl)))
-
-analitico <- bind_cols(
-  "chapter" = c("Relatório analítico"),
-  "data" = partition_020_groups(tbl)) |>
-  filter(info == "Receitas") |>
-  mutate(
-    data = lapply(data, partition_020_receitas))
-
-bind_rows(resumo, analitico) |>
-  select(chapter, info, data)
+tbl <- pcontas_part[2,4][[1]][[1]]
